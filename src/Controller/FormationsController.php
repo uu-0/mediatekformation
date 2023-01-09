102776cf24e96ab77ce780c7f512fbaab4c6cbf5
louise <louisepop14@gmail.com>
2023-01-09 20:53:02 +0100

    Mission 1 : Nettoyer et optimiser le code existant Tâche #1 : Nettoyer le code (2h)

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+define("FORMATIONS", "pages/formations.html.twig");
+
 /**
  * Controleur des formations
  *
@@ -39,7 +41,7 @@ class FormationsController extends AbstractController {
     public function index(): Response{
         $formations = $this->formationRepository->findAll();
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(FORMATIONS, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -55,7 +57,7 @@ class FormationsController extends AbstractController {
     public function sort($champ, $ordre, $table=""): Response{
         $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(FORMATIONS, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -72,7 +74,7 @@ class FormationsController extends AbstractController {
         $valeur = $request->get("recherche");
         $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(FORMATIONS, [
             'formations' => $formations,
             'categories' => $categories,
             'valeur' => $valeur,
@@ -87,7 +89,7 @@ class FormationsController extends AbstractController {
      */
     public function showOne($id): Response{
         $formation = $this->formationRepository->find($id);
-        return $this->render("pages/formation.html.twig", [
+        return $this->render(FORMATIONS, [
             'formation' => $formation
         ]);        
     }   
--- src/Entity/Playlist.php
+++ src/Entity/Playlist.php
@@ -88,11 +88,9 @@ class Playlist
 
     public function removeFormation(Formation $formation): self
     {
-        if ($this->formations->removeElement($formation)) {
+         if ($this->formations->removeElement($formation) && $formation->getPlaylist() === $this) {
             // set the owning side to null (unless already changed)
-            if ($formation->getPlaylist() === $this) {
-                $formation->setPlaylist(null);
-            }
+                $formation->setPlaylist(null);  
         }
 
         return $this;
