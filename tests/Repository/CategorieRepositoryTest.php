16f43c339884e983474d0414c1073a8ef2b15516
louise <louisepop14@gmail.com>
2023-03-16 16:55:53 +0100

    Mission 3 : Tâche 1 : gérer les tests (7h) Test d'Intégration :Contrôler toutes les méthodes ajoutées dans les classes Repository (pour cela, créer une BDD de test) Ici, CategorieRepository #8

---
+++ tests/Repository/CategorieRepositoryTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Tests\Repository;
+
+/**
+ * Description of CategorieRepositoryTest
+ *
+ * @author uu0✿
+ */
+class CategorieRepositoryTest {
+    
+    /**
+     * Récupère le repository de Catégorie
+     */
+    public function recupRepository(): CategorieRepository{
+        self::bootKernel();
+        $repository = self::getContainer()->get(CategorieRepository::class);
+        return $repository;
+    }
+
+    /**
+     * Récupère le nombre d'enregistrements contenus dans la table Catégorie
+     */
+    public function testNbCategories(){
+        $repository = $this->recupRepository();
+        $nbCategories = $repository->count([]);
+        $this->assertEquals(10, $nbCategories);
+    }
+
+    /**
+     * Création d'une instance de Catégorie avec les champs
+     * @return Categorie
+     */
+    public function newCategorie(): Categorie{
+        $categorie = (new Categorie())
+                ->setName("CatégorieTest");
+        return $categorie;
+    }
+
+     public function testAddCategorie(){
+        $repository = $this->recupRepository();
+        $categorie = $this->newCategorie();
+        $nbCategories = $repository->count([]);
+        $repository->add($categorie, true);
+        $this->assertEquals($nbCategories + 1, $repository->count([]), "erreur lors de l'ajout d'une catégorie");
+    }
+
+    public function testRemoveCategorie(){
+        $repository = $this->recupRepository();
+        $categorie = $this->newCategorie();
+        $repository->add($categorie, true);
+        $nbCategories = $repository->count([]);
+        $repository->remove($categorie, true);
+        $this->assertEquals($nbCategories - 1, $repository->count([]), "erreur lors de la suppression d'une catégorie");
+    }
+
+     public function testFindAllForOnePlaylist(){
+        $repository = $this->recupRepository();
+        $categorie = $this->newCategorie();
+        $repository->add($categorie, true);
+        $categories = $repository->findAllForOnePlaylist(3);
+        $nbCategories = count($categories);
+        $this->assertEquals(2, $nbCategories);
+        $this->assertEquals("POO",$categories[0]->getName());
+    }
+}
