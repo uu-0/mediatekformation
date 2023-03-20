e47547d253df389d57b87bfd6e48ab45dd0aa28a
louise <louisepop14@gmail.com>
2023-03-20 13:26:11 +0100

    Mission 3 : Tâche 1 : gérer les tests (7h) Pour PlaylistControllerTest Dans chaque page contenant des listes : - contrôler que les tris fonctionnent (en testant juste le résultat de la première ligne) ; - contrôler que les filtres fonctionnent (en testant le nombre de lignes obtenu et le résultat de la première ligne) ; -  contrôler que le clic sur un lien (ou bouton) dans une liste permet d'accéder à la bonne page (en contrôlant l'accès à la page mais aussi le contenu d'un des éléments de la page). #8

---
+++ tests/Controller/PlaylistControllerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Description of PlaylistControllerTest
+ *
+ * @author uu0✿
+ */
+class PlaylistControllerTest extends WebTestCase{
+    /**
+     * Test acces page Playlist
+     */
+    public function testAccesPage(){
+       $client = static::createClient();
+       $client->request('GET', '/playlists');
+       $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+   }
+   
+   public function testTriPlaylists()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', 'playlists/tri/name/ASC');
+        $this->assertSelectorTextContains('th', 'playlist');
+        $this->assertCount(4, $crawler->filter('th'));
+        $this->assertSelectorTextContains('h5', 'Android - Test playlist');
+    }
+    
+    public function testTriNbFormations()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', 'playlists/tri/nbformations/ASC');
+        $this->assertSelectorTextContains('th', 'playlist');
+        $this->assertCount(4, $crawler->filter('th'));
+        $this->assertSelectorTextContains('h5', 'Cours Informatique embarquée');
+    }
+
+    public function testFiltrePlaylists()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/playlists'); 
+        $crawler = $client->submitForm('filtrer', [
+            'recherche' => 'sujet'
+        ]);
+        //vérifie le nombre de lignes obtenues
+        $this->assertCount(8, $crawler->filter('h5'));
+        // vérifie si la formation correspond à la recherche
+         $this->assertSelectorTextContains('h5', 'sujet');
+    }
+
+    public function testFiltreCategories()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/playlists/recherche/id/categories'); 
+        $crawler = $client->submitForm('filtrer', [
+            'recherche' => 'Android'
+        ]);
+        //vérifie le nombre de lignes obtenues
+        $this->assertCount(3, $crawler->filter('h5'));
+        // vérifie si la formation correspond à la recherche
+         $this->assertSelectorTextContains('h5', 'Android - Test playlist');
+    }
+
+    public function testLinkPlaylists() {
+        $client = static::createClient();
+        $client->request('GET','/playlists');
+        $client->clickLink("Voir détail");
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $uri = $client->getRequest()->server->get("REQUEST_URI");
+        $this->assertEquals('/playlists/playlist/28', $uri);
+    }
+}
